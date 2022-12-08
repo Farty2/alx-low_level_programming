080ed7b06c8b4a0d508285a4744632192b381c9d
Fatiha <fartyaro3@gmail.com>
2022-12-08 14:37:31 -0800

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  * main - prints out "\"programming is like building a multilingual puzzle"
- *
- *   * Return: 0 if it run completely
- *
- *    */
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
+
 }
