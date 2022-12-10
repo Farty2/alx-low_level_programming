7d89d52a3e40fde05a784e0779b32cd64ecb59a8
Fatiha <fartyaro3@gmail.com>
2022-12-09 16:22:36 -0800

    commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print numbers from 0 to 9 separated by , then followed by a space
+ * main - print combinations of numbers from 0-9 separated by , then followed by a space
  * Returns: Always 0 (Success)
  */
 
 int main(void)
 {
 	int num = '0';
+
 	while (num <= '9')
 	{
 		putchar(num);
