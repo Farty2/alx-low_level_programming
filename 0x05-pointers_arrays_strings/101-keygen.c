fa21d95e8096e4da8c43cc6f65e884076a052dda
Fatiha <fartyaro3@gmail.com>
2022-12-19 15:42:44 -0800

    commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -9,16 +9,45 @@
 
 int main(void)
 {
-	int pass, sum;
+	unsigned int long i;
+	int randomer;
 
-	srand(time(NULL));
-	sum = 0;
-	while (sum <= 2645)
+	char lowAlpha[] = "qwertyuiopasdfghjklzxcvbnm";
+	char highAlpha[] = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	char num[] = "0123456789";
+	char sym[] = "!@#$%^&*()~|?<";
+	char password[12];
+
+	srand((unsigned int)(time(NULL)));
+
+	randomer = rand() % 4;
+
+	for (i = 0; i < (sizeof(password) / sizeof(*password)); i++)
 	{
-		pass = (rand() % 128);
-		sum += pass;
-		printf("%c", pass);
+		if (randomer == 1)
+		{
+			password[i] = lowAlpha[rand() % 26];
+			randomer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomer == 2)
+		{
+			password[i] =  highAlpha[rand() % 26];
+			randomer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomer == 3)
+		{
+			password[i] = num[rand() % 10];
+			randomer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = sym[rand() % 14];
+			randomer = rand() % 4;
+			printf("%c", password[i]);
+		}
 	}
-	printf("%c", 2772 - sum);
 	return (0);
 }
