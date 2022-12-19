1fb3cb8814a1c774cfd644cc1f5120e1b98abb2d
Fatiha <fartyaro3@gmail.com>
2022-12-19 13:40:29 -0800

    commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _puts: function that prints a spring, followed by a new line, to stdout
+ * _puts - function that prints a spring, followed by a new line, to stdout
  * @str: pointer value
  * Return: Always 0 (Success)
  */
