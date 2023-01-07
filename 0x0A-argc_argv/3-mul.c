9bac154030d92fd853c8802c12fd22c9b161a94a
Fatiha <fartyaro3@gmail.com>
2023-01-07 05:41:38 -0800

    commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,11 +18,12 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		return (1);
 	}
+	
 	n = atoi(argv[1]);
 	m = atoi(argv[2]);
 	diff = n * m;
 
-	printf("%i\n", diff)
+	printf("%i\n", diff);
 
 		return (0);
 }
