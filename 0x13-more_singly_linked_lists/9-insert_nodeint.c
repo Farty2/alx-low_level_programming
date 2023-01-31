6e85a58b0182df0f75aa010e4aa9c1f0d0c54040
Fatiha <fartyaro3@gmail.com>
2023-01-31 00:07:41 -0800

    commit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,23 +26,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = node;
 		return (node);
 	}
-
 	else if (!*head && idx)
 		return (NULL);
-
 	else if (!idx)
 	{
 		node->next = temp;
 		*head = node;
 		return (node);
 	}
-
 	while (i < (idx - 1))
 	{
 		temp = temp->next;
 		i++;
 	}
-
 	node->next = temp->next;
 	temp->next = node;
 
